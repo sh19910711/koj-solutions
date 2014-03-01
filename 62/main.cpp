b8da90e77c45e475a626bb4083ca4e0884892f8b
Hiroyuki Sano <sh19910711@gmail.com>
2014-03-01 18:12:10 +0900

    KOJ 62: Accepted

---
+++ 62/main.cpp
@@ -0,0 +1,137 @@
+
+// @snippet<sh19910711/contest-base-simple:header.cpp>
+#define __THIS_IS_CPP11__
+/* default headers {{{ */
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <cstdio>
+#include <cmath>
+
+#ifdef __THIS_IS_CPP11__
+#include <memory>
+#include <array>
+#endif
+/* }}} */
+
+
+// @snippet<sh19910711/contest-base-simple:solution/template-typedef.cpp>
+namespace solution {
+  using namespace std;
+  /* primitives {{{ */
+  typedef long long Int;
+  typedef long double Double;
+  /* }}} */
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-constants.cpp>
+namespace solution {
+  const Int SIZE = 1000000 + 11;
+  const string YES = "YES";
+  const string NO = "NO";
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/template-storages.cpp>
+namespace solution {
+  struct Input {
+    Int N;
+    Int M;
+    Int A[SIZE];
+    Int B[SIZE];
+  };
+  
+  struct Output {
+    bool result;
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/solution-io.cpp>
+namespace solution {
+  struct SolutionIO {
+    Input in;
+    Output out;
+    
+    template <class Stream> bool input( Stream& s ) {
+      if ( ! ( s >> in.N >> in.M ) ) return false;
+      for ( auto i = 0; i < in.M; ++ i )
+        if ( ! ( s >> in.A[i] >> in.B[i] ) ) return false;
+      return true;
+    }
+    
+    template <class Stream> bool output( Stream& s ) {
+      return s << ( out.result ? YES : NO ) << endl;
+    }
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/solution.cpp>
+namespace solution {
+  struct Solution: SolutionIO {
+    //
+    // Implement here
+    //
+    
+    Solution() {}
+    
+    bool solve() {
+      out.result = check();
+      return true;
+    }
+
+    bool check() {
+      Int sum = 0;
+      for ( auto i = 0; i < in.N; ++ i ) {
+        sum += in.A[i] * in.B[i];
+      }
+      return sum <= in.N;
+    }
+    
+    /* action methods {{{ */
+    void before_action( const int& t_id ) {}
+    
+    void after_action( const int& t_id ) {}
+    
+    bool action( const int& t_id = -1 ) {
+      before_action(t_id);
+      bool res = input(std::cin) && solve() && output(std::cout);
+      after_action(t_id);
+      return res;
+    }
+    /* }}} */
+  };
+}
+
+// @snippet<sh19910711/contest-base-simple:solution/runner-normal.cpp>
+namespace solution {
+  /* Runner Class {{{ */
+  struct Runner {
+    static int run() {
+      bool f = false;
+      while ( ! f ) {
+        Solution* sol = new Solution;
+        f = ! sol->action();
+        delete sol;
+      };
+      return 0;
+    }
+  };
+  /* }}} */
+}
+
+// @snippet<sh19910711/contest-base-simple:main_func.cpp>
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  return solution::Runner::run();
+}
+
+// @snippet<sh19910711/contest-base-simple:main.cpp>
+
